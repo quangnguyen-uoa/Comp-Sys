66a88638658951749e9af3396bf6e3118f5a8dee
quangnguyen-uoa <a1829913@adelaide.edu.au>
2023-09-19 23:59:43 +0930

    part2

--- prac5/Abs.asm
+++ prac5/Abs.asm
@@ -2,3 +2,19 @@
 // (R0, R1 refer to RAM[0], and RAM[1], respectively.)
 
 // Put your code here.
+@R1
+D = M
+@NEG
+D; JLT
+
+@R0
+M = D
+@END
+0; JMP
+
+(NEG)
+D = -D
+@R0
+M = D
+
+(END)
--- prac5/Mult.asm
+++ prac5/Mult.asm
@@ -7,3 +7,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@R0
+M = 0
+@R1
+D = M
+@STEP
+D; JGT
+
+@END
+0; JMP
+
+(STEP)
+@R0
+D = M
+@R2
+D = D + M
+@R0
+M = D
+@R1
+D = M - 1
+M = D
+@STEP
+D; JGT
+
+(END)
+@END
+0; JMP
+
