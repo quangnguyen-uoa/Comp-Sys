24449814c68a21b1ab915441c274641138a1df70
quangnguyen-uoa <a1829913@adelaide.edu.au>
2023-09-20 22:19:51 +0930

    part3

--- prac5/Mult/Mult.asm
+++ prac5/Mult/Mult.asm
@@ -7,30 +7,108 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
-@R0
-M = 0
+// @R0
+// M = 0
+// @R1
+// D = M
+// @STEP
+// D; JGT
+
+// @END
+// 0; JMP
+
+// (STEP)
+// @R0
+// D = M
+// @R2
+// D = D + M
+// @R0
+// M = D
+// @R1
+// D = M - 1
+// M = D
+// @STEP
+// D; JGT
+
+// (END)
+// @END
+// 0; JMP
+@sgn
+M=0
+(R1_SGN)
 @R1
-D = M
-@STEP
-D; JGT
-
-@END
-0; JMP
-
-(STEP)
-@R0
-D = M
+D=M
+@R1_A
+D;JLT
+(CHECK_R2)
 @R2
-D = D + M
-@R0
-M = D
+D=M
+@R2_A
+D;JLT
+
+(ENTRY)
 @R1
-D = M - 1
-M = D
-@STEP
-D; JGT
+D=M
+@R2
+D=D-M
+@CHANGE
+D;JLT
+
+(LOOP)
+@R2
+MD=M-1
+@CHECK_sgn
+D;JLT
+@R1
+D=M
+@R0
+M=M+D
+@LOOP
+0;JMP
+
+(CHECK_sgn)
+@sgn
+D=M
+@END
+D;JEQ
+@R0
+M=-M
+@END
+0;JMP
 
 (END)
 @END
-0; JMP
+0;JMP
+
+(R2_A)
+@sgn
+M=!M
+@R2
+M=-M
+@ENTRY
+0;JMP
+
+(R1_A)
+@sgn
+M=!M
+@R1
+M=-M
+@CHECK_R2
+0;JMP
+
+(CHANGE)
+@R1
+D=M
+@temp
+M=D
+@R2
+D=M
+@R1
+M=D
+@temp
+D=M
+@R2
+M=D
+@LOOP
+0;JMP
 
--- prac5/Mult/Mult04.tst
+++ prac5/Mult/Mult04.tst
@@ -9,12 +9,12 @@ output-list RAM[0]%D2.6.2 RAM[1]%D2.6.2 RAM[2]%D2.6.2;
 
 set PC 0,
 set RAM[0] 0,  // Set R0
-set RAM[1] 42,  // Set R1
-set RAM[2] 9;  // Set R2
-repeat 1000 {
+set RAM[1] -3,  // Set R1
+set RAM[2] -9;  // Set R2
+repeat 100 {
   ticktock;    // Run for 1000 clock cycles
 }
-set RAM[1] 42,  // Restore arguments in case program used them
-set RAM[2] 9,
+set RAM[1] -3,  // Restore arguments in case program used them
+set RAM[2] -9,
 output;        // Output to file
 
