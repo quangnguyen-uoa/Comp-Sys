e9ea22b2ae5b4f6e0648db035c7016ff4612daa1
quangnguyen-uoa <a1829913@adelaide.edu.au>
2023-10-19 11:49:59 +1030

    p3

--- prac6/VMTranslator/VMTranslator.py
+++ prac6/VMTranslator/VMTranslator.py
@@ -98,7 +98,7 @@ class VMTranslator:
         '''Generate Hack Assembly code for a VM function operation'''
         code = f"({function_name})\n"
         for _ in range(n_vars):
-            code += "@SP\nA=M\nM=0\n@SP\nM=M+1\n"
+            code += "@SP\nAM=M+1\nA=A-1\n@SP\nM=0\n"
         return code
 
     def vm_call(self, function_name, n_args):
